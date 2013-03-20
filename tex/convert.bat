1437b1af3cbf1004f90934715abdee49a01c96ab
Dima Arkhangelskiy <arhangeldim@gmail.com>
2013-03-20 16:57:02 +0400

    Fixed qemu review

--- tex/convert.bat
+++ tex/convert.bat
@@ -1,3 +1,3 @@
-"C:\Program Files (x86)\miktex 2.9\miktex\bin\latex" "C:\Users\arhangeldim\documents\diploma\tex\example.tex"
-"C:\Program Files (x86)\miktex 2.9\miktex\bin\dvipdfm" -p a4 -o ..\diplom.pdf example.dvi
+"C:\Program Files (x86)\miktex 2.9\miktex\bin\latex" "C:\Users\arhangeldim\documents\diploma\tex\header.tex"
+"C:\Program Files (x86)\miktex 2.9\miktex\bin\dvipdfm" -p a4 -o ..\diplom.pdf header.dvi
 @pause
